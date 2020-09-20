c5e0e6bb4c8bca89755fde3490f81f4c0685152f
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-09-20 22:54:37 +0100

    Create main.js

---
+++ docs/main.js
@@ -0,0 +1,42 @@
+function getRandom(max) {
+    return Math.floor((Math.random() * 10) % max)
+}
+
+async function THENEWS() {
+    if(window.hasOwnProperty("bg") && !bg.paused) {
+    clearInterval(TTSKeepAlive);
+    bg.pause();
+    delete TheNewsIntro;
+    speechSynthesis.cancel();
+    bg.currentTime = 0;
+    thenews.innerText = "THE NEWS!";
+    return
+    }
+    TTSKeepAlive = setInterval(_ => {
+    speechSynthesis.pause();
+    speechSynthesis.resume();
+    }, 5000);
+    if (!window.hasOwnProperty("bg")) bg = new Audio("https://news.ndev.tk/bg.mp3");
+    voices = window.speechSynthesis.getVoices().filter(voice => {
+	return voice.lang.startsWith("en-");
+    });
+    voice = voices[getRandom(voices.length)];
+    bg.loop = true;
+    bg.volume = 1;
+    bg.play();
+    thenews.innerText = "Stop Audio";
+    TheNewsIntro = sleep(6000);
+    await TheNewsIntro
+    bg.volume = 0.3;
+    reader();
+}
+
+async function reader() {
+    if(bg.paused) return
+    await generate();
+    text = new SpeechSynthesisUtterance(content.innerText);
+    text.voice = voice;
+    text.onerror = () => reader();
+    text.onend = () => reader();
+    speechSynthesis.speak(text);
+}
